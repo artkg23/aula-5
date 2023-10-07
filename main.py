9a8b741a8fdff3f433872417fad60ee280393187
artkg23 <146586448+artkg23@users.noreply.github.com>
2023-10-07 12:40:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+# cidade ='guarulhos'
+# print(cidade,'/n')
+# frag = list(cidade)
+# # print(frag)
+
+# tr=tuple(frag)
+# print(tr)
+
+# numero=range(1,10)
+# print(numero)
+# lista=[1,6,812,0,45]  
+# soma= sum(lista)
+# print("a soma é",soma)
+
+
+
+  # lista= [1,54,144]
+  # maxi=max(lista)
+  # print(maxi)
+
+# lista =[0,40,1,3,4,56,74,32,3,34,56,7,42,345456,65]
+# ordem =sorted(lista)
+# print(ordem)
+
+# numeros=range(1,20,2)
+# print(numeros)
+
+# a=range(1,5)
+# s=sum(a)
+# print(s)
+
+
+# for i in range(2,21,2):
+#   print(i)
+
+
+
+# name='arthur'
+# print(f'ola,{name})
+# for i in range(1,11,2):
+#  n= range(1,11,2)
+# s=sum(n)/len(n)
+# print(f'a média de {n} é{s})
+
+# lista=['mini pekka','banana','cereja','uva']
+# for i in lista:
+#   print(lista[0])
+
+# nome= {
+#   'nome':'arthur',
+#   'idade':18,
+#   'endereco':'rua 10'
+# }
+# for i1,i2 in dados.items():
+#   print(f' {i1} : {i2} ')
+
+# matriz = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+# for linha in matriz:
+#     for elemento in linha:
+#         print(elemento)
